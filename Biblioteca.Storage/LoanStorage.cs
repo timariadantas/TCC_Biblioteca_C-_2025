bfa4959d09a1db5d24f3b14b66808cf1d2b59286
Maria Dantas <mariadantasti@gmail.com>
2025-08-11 20:58:42 -0300

    adc funcionalidades do Loan

--- Biblioteca.Storage/LoanStorage.cs
+++ Biblioteca.Storage/LoanStorage.cs
@@ -1,14 +1,26 @@
+using System.Data;
+using System.Data.Common;
 using Biblioteca.Domain;
 using Npgsql;
 
 namespace Biblioteca.Storage
 {
-    public class LoanStorage
+    public interface ILoanStorage
+    {
+        void Create(Loan loan);
+        void Delete(string id);
+        List<Loan> GetAll();
+        Loan? GetById(string id);
+        void ReturnLoanAndRestock(string loanId, DateTime? returnDate = null);
+        void Update(Loan loan);
+    }
+
+    public class LoanStorage : ILoanStorage
     {
         public void Create(Loan loan)
         {
             using var conn = DataBase.GetConnection();
-            
+
 
             var cmd = new NpgsqlCommand(@"
                 INSERT INTO loan 
@@ -34,7 +46,7 @@ namespace Biblioteca.Storage
             var loans = new List<Loan>();
 
             using var conn = DataBase.GetConnection();
-            
+
 
             var cmd = new NpgsqlCommand("SELECT * FROM loan", conn);
             using var reader = cmd.ExecuteReader();
@@ -61,7 +73,7 @@ namespace Biblioteca.Storage
         public Loan? GetById(string id)
         {
             using var conn = DataBase.GetConnection();
-            
+
 
             var cmd = new NpgsqlCommand("SELECT * FROM loan WHERE id = @id", conn);
             cmd.Parameters.AddWithValue("id", id);
@@ -90,7 +102,7 @@ namespace Biblioteca.Storage
         public void Update(Loan loan)
         {
             using var conn = DataBase.GetConnection();
-            
+
 
             var cmd = new NpgsqlCommand(@"
                 UPDATE loan SET
@@ -118,12 +130,154 @@ namespace Biblioteca.Storage
         public void Delete(string id)
         {
             using var conn = DataBase.GetConnection();
-            
+
 
             var cmd = new NpgsqlCommand("DELETE FROM loan WHERE id = @id", conn);
             cmd.Parameters.AddWithValue("id", id);
 
             cmd.ExecuteNonQuery();
+        }
+
+
+        //---- devolução ---
+
+        public void ReturnLoanAndRestock(string loanId, DateTime? returnDate = null)
+        {
+            using var conn = DataBase.GetConnection();
+            if (conn.State != ConnectionState.Open) conn.Open();
+
+            using var tx = conn.BeginTransaction();
+
+            try
+            {
+                var selectCmd = new NpgsqlCommand("SELECT inventory_id, status FROM loan WHERE id = @id FOR UPDATE", conn, tx);
+                selectCmd.Parameters.AddWithValue("id", loanId);
+                string InventoryId;
+                string status;
+                using (var reader = selectCmd.ExecuteReader())
+                {
+                    if (!reader.Read())
+                    {
+                        throw new Exception("Emprestimo não localizado.");
+
+                    }
+                    InventoryId = reader.GetString(0);
+                    status = reader.GetString(1);
+
+                }
+
+                if (status == "Devolvido")
+                    throw new Exception("Este empréstimo já foi devolvido");
+
+                var rd = returnDate ?? DateTime.UtcNow;
+
+                //--> Atualizar
+                var upLoan = new NpgsqlCommand(@"
+                UPDATE loan 
+                SET return_date = @return_date,
+                status = @status,
+                updated_at = @updated_at
+                WHERE id = @id", conn, tx);
+
+                upLoan.Parameters.AddWithValue("return_date", rd);
+                upLoan.Parameters.AddWithValue("status", "Devolvido");
+                upLoan.Parameters.AddWithValue("updated_at", DateTime.UtcNow);
+                upLoan.Parameters.AddWithValue("id", loanId);
+
+                var rowsUpdated = upLoan.ExecuteNonQuery();
+                if (rowsUpdated == 0)
+                    throw new Exception("Falha ao atualizar o empréstimo.");
+
+                //--> Incrementar quantidade no inventário
+
+                var upInv = new NpgsqlCommand(@"
+                UPDATE Inventory
+                SET quantity = quantity + 1,
+                    updated_at = @updated_at
+                WHERE id = @id", conn, tx);
+
+                upInv.Parameters.AddWithValue("updated_at", DateTime.UtcNow);
+                upInv.Parameters.AddWithValue("id", InventoryId);
+
+                var rowsInv = upInv.ExecuteNonQuery();
+                if (rowsInv == 0)
+                    throw new Exception("Inventário não encontrado");
+
+                tx.Commit();
+                Console.Write("Devolução registrada e inventário atualizado");
+            }
+            catch (Exception ex)
+            {
+                tx.Rollback();
+                Console.WriteLine($"Erro ao registrar devolução: {ex.Message}");
+                throw;
+            }
+
+        }
+
+        public void ListActiveLoans()
+        {
+            using var conn = DataBase.GetConnection();
+
+            var sql = @"
+            SELECT  id, client_id, inventory_id, status, loan_date
+            FROM loan
+            WHERE status <> 'Devolvido'
+            ORDER BY loan_date DESC";
+
+            using var cmd = new NpgsqlCommand(sql, conn);
+            using var reader = cmd.ExecuteReader();
+
+            Console.WriteLine("\n ---> Empréstimos Ativos");
+            while (reader.Read())
+            {
+                string id = reader.GetString(0);
+                string clientId = reader.GetString(1);
+                string inventoryId = reader.GetString(2);
+                string status = reader.GetString(3);
+                DateTime loanDate = reader.GetDateTime(4);
+
+                Console.WriteLine($"ID: {id} | Cliente: {clientId} | Item: {inventoryId} | Status: {status} | Data: {loanDate: dd/MM/yyyy}");
+            }
+        }
+
+
+        public List<Loan> GetLoansByClient(string clientId)
+        {
+            var loans = new List<Loan>();
+
+            using var conn = DataBase.GetConnection();
+            
+
+            var sql = @"
+        SELECT id, client_id, inventory_id, loan_date, due_date, return_date, status, created_at, updated_at
+        FROM loan
+        WHERE client_id = @client_id
+        ORDER BY loan_date DESC";
+
+            using var cmd = new NpgsqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("client_id", clientId);
+
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                loans.Add(new Loan
+                {
+                    Id = reader.GetString(0),
+                    ClientId = reader.GetString(1),
+                    InventoryId = reader.GetString(2),
+                    LoanDate = reader.GetDateTime(3),
+                    DueDate = reader.GetDateTime(4),
+                    ReturnDate = reader.IsDBNull(5) ? null : reader.GetDateTime(5),
+                    Status = reader.GetString(6),
+                    CreatedAt = reader.IsDBNull(7) ? (DateTime?)null : reader.GetDateTime(7),
+                    UpdatedAt = reader.IsDBNull(8) ? (DateTime?)null : reader.GetDateTime(8)
+                });
+            }
+
+            return loans;
+
         }
     }
 }
