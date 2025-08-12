d601dda0bfda18db552f7fad0bb616174ba1847b
Maria Dantas <mariadantasti@gmail.com>
2025-08-11 21:01:34 -0300

    adc funcionalidades do Loan

--- Biblioteca.API/Program.cs
+++ Biblioteca.API/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Biblioteca.API;
 using Biblioteca.Services;
+using Biblioteca.Storage;
 
 
 
@@ -7,14 +8,19 @@ var menu = new Menu();
 var catalogApi = new CatalogApi();
 var inventoryApi = new InventoryApi(new InventoryService());
 var loanApi = new LoanApi(new LoanService());
+var ListClient = new LoanStorage();
 
 while (true)
 {
-    Console.WriteLine("\n=== SISTEMA DA BIBLIOTECA MARIA ===");
+    Console.WriteLine("\n=== SEJA BEM VINDO À BIBLIOTECA DA MARIA ===");
     Console.WriteLine("1 - Gerenciar Clientes");
     Console.WriteLine("2 - Gerenciar Catálogo");
     Console.WriteLine("3 - Gerenciar Inventário");
     Console.WriteLine("4 - Gerenciar Emprestimo ");
+    Console.WriteLine("5 - Devolver");
+    Console.WriteLine("6 - Listar Cliente");
+    Console.WriteLine("7 - Relatório");
+
     Console.WriteLine("0 - Sair");
 
     Console.Write("Escolha uma opção: ");
@@ -34,7 +40,16 @@ while (true)
             case "4":
             loanApi.Menu();
             break;
-        case "0":
+            case "5":
+            loanApi.Devolver();
+            break;
+            case "6":
+            ListClient.ListActiveLoans();
+            break;
+            case "7":
+            loanApi.RelatorioEmprestimosPorCliente();
+            break;
+            case "0":
             return;
         default:
             Console.WriteLine("Opção inválida. Tente novamente.");
